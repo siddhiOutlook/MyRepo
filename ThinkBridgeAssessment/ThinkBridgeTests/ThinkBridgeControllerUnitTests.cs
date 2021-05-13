003f5fcb28b5cdd79e5b2046daa22e81d5303a75
siddhiOutlook <84133624+siddhiOutlook@users.noreply.github.com>
2021-05-13 18:46:43 +0530

    Adding test files

---
+++ ThinkBridgeAssessment/ThinkBridgeTests/ThinkBridgeControllerUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkBridgeAssessment.Controllers;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace ThinkBridgeTests
+{
+    [TestClass]
+    class ThinkBridgeControllerUnitTests
+    {
+        #region Private members
+        private ItemController _itemController = new ItemController();
+        #endregion
+
+        #region Initialize
+        [TestInitialize]
+        public void Initialize()
+        {
+            var httpResponseMessage = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent("This is result")
+            };
+        }
+        #endregion
+
+        #region Tests
+        [TestMethod]
+        public void ItemContollerGetItem_Test()
+        {
+            IHttpActionResult result = _itemController.GetItem();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ItemContollerNewItem_Test()
+        {
+            HttpResponseMessage result = _itemController.NewItem("Chocolate","Dark",true);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ItemContollerRemoveItem_Test()
+        {
+            HttpResponseMessage result = _itemController.RemoveItem("Pen");
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ItemContollerModifyItem_Test()
+        {
+            HttpResponseMessage result = _itemController.ModifyItem("Chocolate", false);
+            Assert.IsNotNull(result);
+        }
+        #endregion
+    }
+}
---
+++ ThinkBridgeAssessment/ThinkBridgeTests/ThinkBridgeRepositoryUnitTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThinkBridgeAssessment.Entity;
+using ThinkBridgeAssessment.Repository;
+
+namespace ThinkBridgeTests
+{
+    [TestClass]
+    public class ThinkBridgeRepositoryUnitTests
+    {
+        #region Private Menbers
+        private IInventory _inventory = new Inventory();
+        #endregion
+
+        #region Initialize
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+        #endregion
+
+        #region Mock helpers
+        #endregion
+
+        #region Tests
+        [TestMethod]
+        public void InventoryGet_Test()
+        {
+            var result = _inventory.Get();
+            Assert.IsNotNull(result);
+            //Assert.IsTrue(result is JsonResult);
+        }
+
+        [TestMethod]
+        public void InventoryAddItem_Test()
+        {
+            Item i = new Item(0,"Icecream", "mango", true);
+            var result = _inventory.AddItem(i);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Name, "Icecream");
+        }
+        [TestMethod]
+        public void InventoryUpdateItem_Test()
+        {
+            Item i = new Item(0, "Pen", "Green", false);
+            var result = _inventory.UpdateItem(i);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.isAvailable, false);
+        }
+        [TestMethod]
+        public void InventoryRemoveItem_Test()
+        {
+            Item i = new Item(0, "Notepad", "100 pages", true);
+            var result = _inventory.RemoveItem(i);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Name, "Notepad");
+        }
+        #endregion
+    }
+}
