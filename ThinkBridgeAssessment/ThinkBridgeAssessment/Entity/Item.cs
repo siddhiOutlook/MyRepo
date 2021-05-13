a281e1a086843a43b9fef6dad668d557232c47d5
siddhiOutlook <84133624+siddhiOutlook@users.noreply.github.com>
2021-05-13 18:56:20 +0530

    Add files via upload

---
+++ ThinkBridgeAssessment/ThinkBridgeAssessment/Entity/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThinkBridgeAssessment.Entity
+{
+    public class Item
+    {
+        public Item(int id, string name, string description, bool available)
+        {
+            itemId = id;
+            Name = name;
+            Description = description;
+            isAvailable = available;
+        }
+        public int itemId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool isAvailable { get; set; }
+    }
+}
